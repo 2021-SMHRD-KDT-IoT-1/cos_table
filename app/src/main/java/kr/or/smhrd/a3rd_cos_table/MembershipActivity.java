6529c6234fbeef18919e3542082b2c9683683213
kyj9373 <kyj9953@naver.com>
2021-07-28 09:03:39 +0900

    membership, logo, skintest xml

---
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
@@ -0,0 +1,14 @@
+package kr.or.smhrd.a3rd_cos_table;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MembershipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_membership);
+    }
+}
