89af874dc940396795e368d275008ee727ec9905
kyj9373 <kyj9953@naver.com>
2021-07-28 15:28:55 +0900

    membership, skintest 초기화 membership toggle 구현중

--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
@@ -2,13 +2,102 @@ package kr.or.smhrd.a3rd_cos_table;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MembershipActivity extends AppCompatActivity {
+    EditText edt_mem_id,edt_mem_pw,edt_mem_pwck;
+    TextView tv_skintype,tv_ck_mytype,tv_ck_sensitive,tv_Q1,tv_Q2;
+    Button btn_dry,btn_normal,btn_oily,btn_combi,btn_mem_join;
+
+    boolean isClicked=false;
+    //boolean isClicked=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membership);
+
+        edt_mem_id=findViewById(R.id.edt_mem_id);
+        edt_mem_pw=findViewById(R.id.edt_mem_pw);
+        edt_mem_pwck=findViewById(R.id.edt_mem_pwck);
+
+        tv_skintype=findViewById(R.id.tv_skintype);
+        tv_ck_mytype=findViewById(R.id.tv_ck_mytype);
+        tv_ck_sensitive=findViewById(R.id.tv_ck_sensitive);
+
+        btn_dry=findViewById(R.id.btn_dry);
+        btn_normal=findViewById(R.id.btn_normal);
+        btn_oily=findViewById(R.id.btn_oily);
+        btn_combi=findViewById(R.id.btn_combi);
+        btn_mem_join=findViewById(R.id.btn_mem_join);
+
+//        // 내피부타입
+//        tv_ck_mytype.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//               // Intent intent= new Intent(this,SkintestActivity.class);
+//
+//
+//            }
+//        });
+//
+//        //타입별 토글버튼 구현
+//        btn_dry.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(!isClicked){
+//                    btn_dry.setBackgroundColor(Color.CYAN);
+//                }else{
+//                    //btn_dry.setBackgroundColor(Color.GRAY);
+//                }
+//            }
+//        });
+//
+//        btn_normal.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(isClicked==false){
+//                    isClicked=true;
+//                    btn_normal.setBackgroundColor(Color.CYAN);
+//                }else{
+//                    isClicked=false;
+//                    btn_normal.setBackgroundColor(Color.GRAY);
+//                }
+//            }
+//        });
+//
+//        btn_oily.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(isClicked==false){
+//                    isClicked=true;
+//                    btn_oily.setBackgroundColor(Color.CYAN);
+//                }else{
+//                    isClicked=false;
+//                    btn_oily.setBackgroundColor(Color.GRAY);
+//                }
+//            }
+//        });
+//
+//        btn_combi.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(isClicked==false){
+//                    isClicked=true;
+//                    btn_combi.setBackgroundColor(Color.CYAN);
+//                }else{
+//                    isClicked=false;
+//                    btn_combi.setBackgroundColor(Color.GRAY);
+//                }
+//            }
+//        });
+
     }
 }
--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/SkintestActivity.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/SkintestActivity.java
@@ -3,12 +3,22 @@ package kr.or.smhrd.a3rd_cos_table;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SkintestActivity extends AppCompatActivity {
-
+    TextView tv_test_skin;
+    Button btn_test_no,btn_test_back,btn_test_yes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skintest);
+
+        tv_test_skin=findViewById(R.id.tv_test_skin);
+        btn_test_no=findViewById(R.id.btn_test_no);
+        btn_test_back=findViewById(R.id.btn_test_back);
+        btn_test_yes=findViewById(R.id.btn_test_yes);
+
+
     }
 }
