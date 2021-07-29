831d39428604ddfb910dcaa3b9a38aae4ac929c4
kyj9373 <kyj9953@naver.com>
2021-07-29 19:16:55 +0900

    cos_login에 기능 구현중, main->화장품 등록시 cos_add화면 이동구현

--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/cos_login.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/cos_login.java
@@ -7,11 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class cos_login extends AppCompatActivity {
 
     EditText edt_login_id, edt_login_pw;
     Button btn_login;
+
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +36,44 @@ public class cos_login extends AppCompatActivity {
         edt_login_pw = findViewById(R.id.edt_login_pw);
         btn_login = findViewById(R.id.btn_login);
 
+        queue= Volley.newRequestQueue(getApplicationContext());
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id=edt_login_id.getText().toString();
-                String pw=edt_login_pw.getText().toString();
+                //String id=edt_login_id.getText().toString();
+               // String pw=edt_login_pw.getText().toString();
 
+                String login_url="http://121.147.0.224:8081/AndServer/LoginService";
 
-                if(id.equals("smhrd")&& pw.equals("1234")) {
+                StringRequest request= new StringRequest(Request.Method.POST, login_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
 
-                    //Intent 객체 생성
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                if(response.equals("0")){
+                                    Toast.makeText(cos_login.this, "로그인 실패..", Toast.LENGTH_SHORT).show();
 
-                    //정보저장
-                    intent.putExtra("id",id);
-                    startActivity(intent);
-                }
+                                }else{
+                                    Toast.makeText(cos_login.this, "로그인 성공!!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params=new HashMap<>();
+                        params.put("login_id",edt_login_id.getText().toString());
+                        params.put("login_pw",edt_login_pw.getText().toString());
+
+                        return params;
+                    }
+                };
+                queue.add(request);
             }
         });
 
