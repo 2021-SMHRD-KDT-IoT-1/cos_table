06628a8fb3e74d8ef745f6f26bdc53e08c577306
rhksrb99 <rhksrb99@gmail.com>
2021-07-29 09:46:34 +0900

    관규 - 메인페이지 라디오 버튼 구현중22222

--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -15,13 +16,9 @@ import android.widget.ToggleButton;
 public class MembershipActivity extends AppCompatActivity {
     EditText edt_mem_id,edt_mem_pw,edt_mem_pwck;
     TextView tv_skintype,tv_ck_mytype,tv_ck_sensitive,tv_Q1,tv_Q2;
-    Button btn_dry,btn_normal,btn_oily,btn_combi,btn_mem_join;
-    ToggleButton tg_btn_dry;
+    Button btn_mem_join;
+    RadioButton rd_btn_dry,rd_btn_oily,rd_btn_normal,rd_btn_multi;
 
-
-
-    boolean isClicked=false;
-    //boolean isClicked=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +33,6 @@ public class MembershipActivity extends AppCompatActivity {
         tv_ck_mytype=findViewById(R.id.tv_ck_mytype);
         tv_ck_sensitive=findViewById(R.id.tv_ck_sensitive);
 
-//        btn_dry=findViewById(R.id.btn_dry);
-//        btn_normal=findViewById(R.id.btn_normal);
-//        btn_oily=findViewById(R.id.btn_oily);
-//        btn_combi=findViewById(R.id.btn_combi);
         btn_mem_join=findViewById(R.id.btn_mem_join);
 
 
@@ -51,70 +44,7 @@ public class MembershipActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        // -------------------------------------------------------------
-
-//        //타입별 토글버튼 구현
-//        btn_dry.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(!isClicked){
-//                    btn_dry.setBackgroundColor(Color.CYAN);
-//                }else{
-//                    //btn_dry.setBackgroundColor(Color.GRAY);
-//                }
-//            }
-//        });
-//
-//        btn_normal.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(isClicked==false){
-//                    isClicked=true;
-//                    btn_normal.setBackgroundColor(Color.CYAN);
-//                }else{
-//                    isClicked=false;
-//                    btn_normal.setBackgroundColor(Color.GRAY);
-//                }
-//            }
-//        });
-//
-//        btn_oily.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(isClicked==false){
-//                    isClicked=true;
-//                    btn_oily.setBackgroundColor(Color.CYAN);
-//                }else{
-//                    isClicked=false;
-//                    btn_oily.setBackgroundColor(Color.GRAY);
-//                }
-//            }
-//        });
-//
-//        btn_combi.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(isClicked==false){
-//                    isClicked=true;
-//                    btn_combi.setBackgroundColor(Color.CYAN);
-//                }else{
-//                    isClicked=false;
-//                    btn_combi.setBackgroundColor(Color.GRAY);
-//                }
-//            }
-//        });
-
     }
-    public void onToggleClicked(View v){
-        boolean on = ((ToggleButton)v).isChecked();
-
-        if (on){
-            Toast.makeText(getApplicationContext(), "on", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(getApplicationContext(), "off", Toast.LENGTH_SHORT).show();
-        }
-    }
-
 }
 
 
