13d803046313f3f9c4acb85b0a09a0e868cbdc43
woyeonggwang <zerolight9@naver.com>
2021-07-29 09:35:29 +0900

    지금 하단바로 프래그먼트띄워서 피부특성을 확인하도록 하는 그런 기능 제작중입니다

---
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/SkinTypeFrag/frg_Q1.java
@@ -0,0 +1,27 @@
+package kr.or.smhrd.a3rd_cos_table.SkinTypeFrag;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import kr.or.smhrd.a3rd_cos_table.R;
+
+public class frg_Q1 extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_frg__q1, container, false);
+
+
+//        return view;
+        return null;
+    }
+}
