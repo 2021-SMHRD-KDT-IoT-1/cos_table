85def9e78fa890d2cd174a9497b27dfe2a3d4d93
woyeonggwang <zerolight9@naver.com>
2021-07-31 08:54:18 +0900

    0731아침커풀푸 이미지클릭리스너 조금 추가됨

--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/CosAddActivity.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/CosAddActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import kr.or.smhrd.a3rd_cos_table.cos_img.img_1st;
 
 public class CosAddActivity extends AppCompatActivity {
 
     Button btn_add, btn_edt, btn_delete;
+    ImageView img_1st_add, img_2nd_add, img_3rd_add, img_4th_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,12 @@ public class CosAddActivity extends AppCompatActivity {
         btn_edt = findViewById(R.id.btn_edt);
         btn_delete = findViewById(R.id.btn_delete);
 
+        img_1st_add=findViewById(R.id.img_1st_add);
+        img_2nd_add=findViewById(R.id.img_2nd_add);
+        img_3rd_add=findViewById(R.id.img_3rd_add);
+        img_4th_add=findViewById(R.id.img_4th_add);
+
+
         // 등록 버튼 클릭 시 Cos_Shoot페이지로 이동
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +40,28 @@ public class CosAddActivity extends AppCompatActivity {
                 Intent intent = new Intent(CosAddActivity.this, Cos_ShootActivity.class);
                 startActivity(intent);
             }
+        });
+
+        img_1st_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
+
+                getMenuInflater().inflate(R.menu.img1_add, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        if (item.getItemId()==R.id.action_menu1) {
+                            Toast.makeText(CosAddActivity.this, "메뉴1클릭",Toast.LENGTH_SHORT).show();
+                        }//이미지클릭리스너 만드는중이에요
+
+                        return false;
+                    }
+                });
+            }
+
+
         });
 
     }
---
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/cos_img/img_1st.java
@@ -0,0 +1,19 @@
+package kr.or.smhrd.a3rd_cos_table.cos_img;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.or.smhrd.a3rd_cos_table.R;
+
+public class img_1st extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img1st);
+
+
+        
+    }
+}
