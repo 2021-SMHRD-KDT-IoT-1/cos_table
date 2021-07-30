0d873c6c56ce72372b8bf1c0e839ed316d5e4f0c
woyeonggwang <zerolight9@naver.com>
2021-07-30 15:06:50 +0900

    Cos_shot 액티비티 큐알찍는거 추가했어요 오류가 있긴한데 이름 안 바꿔서 그런거니 신경안써도 돼요!!!!

--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/Cos_ShootActivity.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/Cos_ShootActivity.java
@@ -6,11 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Cos_ShootActivity extends AppCompatActivity {
     TextView tv_cosshoot,tv_pdqr;
     Button btn_cancle,btn_shoot;
+    ImageView img_ex2;
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +31,19 @@ public class Cos_ShootActivity extends AppCompatActivity {
         tv_pdqr=findViewById(R.id.tv_pdqr);
         btn_cancle=findViewById(R.id.btn_cancle);
         btn_shoot=findViewById(R.id.btn_shoot);
+        img_ex2=findViewById(R.id.img_ex2);
+
+        //qr스캔 정의
+        qrScan = new IntentIntegrator(this);
 
         // 촬영 버튼 클릭시 qr촬영 액티비티(영광이형 제작 중)로 이동 후 촬영 완료 시 Cos_detail로 정보와 함께 이동
         btn_shoot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                //scan option
+                qrScan.setPrompt("Scanning...");
+                //qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
             }
         });
 
@@ -40,4 +57,34 @@ public class Cos_ShootActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //qrcode 가 없으면
+            if (result.getContents() == null) {
+                Toast.makeText(Cos_ShootActivity.this, "취소!", Toast.LENGTH_SHORT).show();
+            } else {
+                //qrcode 결과가 있으면
+                Toast.makeText(Cos_ShootActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Cos_ShootActivity.this, Cos_DetailActivity.class);//Cos_DetailActvity로 보내줌
+                try {
+                    //data를 json으로 변환
+                    JSONObject obj = new JSONObject(result.getContents());
+                    textViewName.setText(obj.getString("name"));
+                    textViewAddress.setText(obj.getString("address"));
+                }catch (JSONException e) {
+                    e.printStackTrace();
+                    //Toast.makeText(MainActivity.this, result.getContents(), Toast.LENGTH_LONG).show();
+                    textViewResult.setText(result.getContents());
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 }
