693ab0534e8dd74312501a944dec92c4e940d390
rhksrb99 <rhksrb99@gmail.com>
2021-07-31 15:54:56 +0900

    관규 - 메인 민감성 버튼 수정

--- app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
+++ app/src/main/java/kr/or/smhrd/a3rd_cos_table/MembershipActivity.java
@@ -29,10 +29,10 @@ import java.util.Map;
 
 public class MembershipActivity extends AppCompatActivity {
     EditText edt_mem_id,edt_mem_pw,edt_mem_pwck;
-    TextView tv_skintype,tv_ck_mytype,tv_ck_sensitive,tv_Q1,tv_Q2;
+    TextView tv_skintype,tv_ck_mytype,tv_ck_sensitive;
     Button btn_mem_join;
-    RadioGroup rd_btn_group;
-    RadioButton rd_btn_dry, rd_btn_oily, rd_btn_normal, rd_btn_multi;
+    RadioGroup rd_btn_group, rd_sen_group;
+    RadioButton rd_btn_dry, rd_btn_oily, rd_btn_normal, rd_btn_multi, rd_sen_Q1, rd_sen_Q2;
     RequestQueue queue;
 
     private String rd_result;
@@ -57,6 +57,10 @@ public class MembershipActivity extends AppCompatActivity {
         rd_btn_oily=findViewById(R.id.rd_btn_oily);
         rd_btn_normal=findViewById(R.id.rd_btn_normal);
         rd_btn_multi=findViewById(R.id.rd_btn_multi);
+
+        rd_sen_group = findViewById(R.id.rd_sen_group);
+        rd_sen_Q1 = findViewById(R.id.rd_sen_Q1);
+        rd_sen_Q2 = findViewById(R.id.rd_sen_Q2);
 
         queue = Volley.newRequestQueue(getApplicationContext());
 
@@ -85,6 +89,18 @@ public class MembershipActivity extends AppCompatActivity {
             }
         });
 //=========================================================================================================
+//        //라디오버튼 값 넘기기위한 이벤트
+        rd_sen_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rd_sen_Q1){
+                    rd_result = rd_sen_Q1.getText().toString();
+                }else {
+                    rd_result = rd_sen_Q2.getText().toString();
+                }
+            }
+        });
+//=========================================================================================================
         //회원가입 버튼 클릭 시 피부타입이 null값이라면 토스트 메시지 출력!
 //        btn_mem_join.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -109,7 +125,7 @@ public class MembershipActivity extends AppCompatActivity {
                 //입력 비밀번호가 같은지 체크
                 if (pw.equals(pwck)) {
 
-                    String join_url = "http://121.147.0.224:8081/AndServer/JoinService";
+                    String join_url = "http://220.71.97.208:8099/AndServer/JoinService";
 
                     StringRequest request = new StringRequest(Request.Method.POST, join_url,
                             new Response.Listener<String>() {
